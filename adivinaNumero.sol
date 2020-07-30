81d4b517ce215c0cfa319ef1c1fd1c4a7f0ffdd6
Víctor Sánchez <57121207+VikSanz@users.noreply.github.com>
2020-07-30 13:06:44 -0500

    Update adivinaNumero.sol

--- adivinaNumero.sol
+++ adivinaNumero.sol
@@ -1,25 +1,60 @@
 pragma solidity > 0.6.1 < 0.7.0;
 
-contract AdivinaNumero {
+import "github.com/provable-things/ethereum-api/provableAPI_0.6.sol";
+
+contract AdivinaNumero is usingProvable{
     
    address private jugador;
    uint premio;
-   uint numeroAleatorio;
+   uint private randomnumber;
+   uint private constant costoJuego = 200000000000000000; /// 0.2 ether
+   address private owner;
+   bool private jugando = false;
    
-   event Numero(uint randomnumber, uint numeroAdivinado, uint premio);
+   /// Posteriormente, Desplegaremos un mensaje al jugador una vez que hagamos una interfaz de usuario.
+   event Numeros(string, uint randomnumber, string, uint numeroAdivinado, string ,uint premio);
    
-   function adivina(uint numeroAdivinado) public payable {
+   /// Constructor
+   constructor() public payable{
+       owner = msg.sender;
+       //// Verifica que el numero aleatorio sea seguro
+       provable_setProof(proofType_Ledger);
+   }
+   
+   function adivina() public payable {
        
-      uint randomnumber = uint(keccak256(abi.encodePacked(now,msg.sender))) % 10 + 1;
-      
-      if(numeroAdivinado == randomnumber){
-          
-          jugador = msg.sender;
-          premio = 1000000000000000000; // Premio es un Ether
-      }
-    
-    emit Numero(randomnumber,numeroAdivinado,premio);
+       require(msg.value == 200000000000000000, "El costo del juego es de 0.2 ether." );
+       require(!jugando, "Actualmente hay otro jugador, espero un momento para jugar.");
+       
+       /// El O corrsponde al QUERY_EXECUTION_DELAY, y siempre sera 0 pues queremos el número ASAP
+       /// 1 es la cantidad de bytes que pedimos, en este caso es 1 pues solo queremos un número
+       /// 200000 el Gas necesario para ejecutar la función callback
+       provable_newRandomDSQuery(0, 1, 200000); 
+  
    }
     
+    //Sobreescritura del callback para la API de Provable.
+    function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public override {
+        require(msg.sender == provable_cbAddress());
+        require(provable_randomDS_proofVerify__returnCode(_queryId, _result, _proof) == 0, "Uno de los números aleatorios generados no es confiable.");
+        /// Generamos el número aleatorio
+        randomnumber = ((uint256(keccak256(abi.encodePacked(_result))) % 10) + 1);
+    }
+    
+    /// Tenemos esta otra función que guarda la lógica del juego pues el número aleatorio se genera de manera asíncrona.
+   function jugar( uint numeroAdivinado) public payable {
+       
+       /// Gas para generar los números aleatorios
+       require((msg.value == 200000), "Para jugar envíe 200000 wei." );
+       
+       if(numeroAdivinado == randomnumber){
+            jugador = msg.sender;
+            premio = 1000000000000000000;
+        }
+        else{
+            premio = 0;
+        }
+        /// Desplegamos el mensaje
+        emit Numeros("El numero generado fue:",randomnumber,"Tu escogiste:",numeroAdivinado,"Ganaste:", premio);
+   }
 }
-
